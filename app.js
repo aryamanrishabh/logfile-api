3718df2e848de85ffaf445445b7b730d349fdf75
aryamanrishabh <aryamanrishabh@gmail.com>
2021-06-03 12:20:16 +0530

    commit on master

--- app.js
+++ app.js
@@ -1,21 +1,58 @@
+const http = require("http");
+const url = require("url");
 const fs = require("fs");
-const fsPromises = fs.promises;
+const querystring = require("querystring");
 
-const buffer = Buffer.alloc(1024);
+const file = "example.txt";
 
-console.log(fs.readFileSync("test.txt", "utf-8"));
+const stream = fs.createReadStream(file, {
+  flags: "r",
+  encoding: "utf-8",
+  fd: null,
+  mode: "0666",
+  bufferSize: 64 * 1024,
+});
 
-async function doRead() {
-  let filehandle = null;
+let fileData = "";
+let lines;
 
-  try {
-    filehandle = await fsPromises.open("test.txt", "r+");
-    await filehandle.read(buffer, 0, buffer.length, 3);
-  } finally {
-    if (filehandle) {
-      await filehandle.close();
-    }
+stream.on("data", function (data) {
+  fileData += data;
+
+  lines = fileData.split("\n");
+});
+
+const server = http.createServer((req, res) => {
+  const urlparse = new URL(req.url, "http://localhost:7000/");
+
+  if (urlparse.pathname == "/logs" && req.method == "GET") {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end(fileData);
   }
-}
+  if (urlparse.pathname == "/logs/timeframe" && req.method == "GET") {
+    let start, end;
+    let start_time, end_time;
+    let output = "";
 
-doRead().catch(console.error);
+    const search = urlparse.search;
+    let result;
+
+    if (search) {
+      const [, query] = urlparse.search.split("?");
+      start = parseInt(querystring.parse(query).start);
+      end = parseInt(querystring.parse(query).end);
+
+      result = lines.filter(
+        (line) =>
+          Date.parse(line.split(" ")[0]) > start &&
+          Date.parse(line.split(" ")[0]) < end
+      );
+      output = result.join("\n");
+    }
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end(output);
+  }
+});
+
+server.listen(7000);
+console.log('Server is running on port 7000...')
